c0fa3ec87b2692534844bf72664c0ba873c324c3
Akshitha-ns <akshithanagaraj16@gmail.com>
2026-03-28 10:59:44 +0530

    Add SpeakGoodBye.js to say goodbye

---
+++ SpeakGoodBye.js
@@ -0,0 +1,12 @@
+(function (window) {
+  var byeSpeaker = {};
+
+  byeSpeaker.speakWord = "Goodbye";
+
+  byeSpeaker.speak = function (name) {
+    console.log(byeSpeaker.speakWord + " " + name);
+  };
+
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
